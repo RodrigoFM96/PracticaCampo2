586e39f77e1077c066cb374b0a36731d8909eb69
RodrigoFM96 <rodrigofrancomendoza4696@gmail.com>
2025-09-17 21:19:38 -0500

    se añadió comentarios

--- Estructura_Seleccion_Multiple/Program.cs
+++ Estructura_Seleccion_Multiple/Program.cs
@@ -10,10 +10,13 @@ namespace Estructura_Seleccion_Multiple
     {
         static void Main(string[] args)
         {
+            //vareable para almacenar el saldo inicial y la opcion elegida por el usuario
             double saldo = 1000;
             int opcion;
+            //ciclo do-while: se repetira hasta llegar a la opcion 4
             do
             {
+            //Menu de opciones que el usuario eligira
                 Console.WriteLine("--- Cajero Automático ---");
                 Console.WriteLine("1. Consultar saldo");
                 Console.WriteLine("2. Depositar dinero");
@@ -21,32 +24,40 @@ namespace Estructura_Seleccion_Multiple
                 Console.WriteLine("4. Salir");
                 Console.Write("Seleccione una opción: ");
                 opcion = int.Parse(Console.ReadLine());
-
+                
+               //depende de la opcion elegida se ejecutan los casos
                 switch (opcion)
                 {
+                        //aqui muestra el saldo inicial que es 1000
                     case 1:
                         Console.WriteLine($"Su saldo actual es: {saldo}");
                         break;
-                    case 2:
+                    case 2: 
+                        //aqui muestra la cantidad a depositar que se le sumara al saldo inicial
                         Console.Write("Ingrese monto a depositar: ");
                         double deposito = double.Parse(Console.ReadLine());
                         saldo += deposito;
                         Console.WriteLine("Depósito realizado con éxito.");
                         break;
                     case 3:
+                        // aqui el usuario debe ingresar la cantidad a retirar, no debe ser mayor al saldo 
                         Console.Write("Ingrese monto a retirar: ");
                         double retiro = double.Parse(Console.ReadLine());
                         if (retiro <= saldo)
                         {
+                            // si el monto del retiro es menor, se descontara del saldo 
                             saldo -= retiro;
+                            // aqui indicara que el retiro fue aceptado.
                             Console.WriteLine("Retiro exitoso.");
                         }
                         else
+                            //caso contrario se mostrara saldo insuficiente
                         {
                             Console.WriteLine("Saldo insuficiente.");
                         }
                         break;
                     case 4:
+                        //mensaje de despedida, al salir 
                         Console.WriteLine("Gracias por usar el cajero,vuelva pronto.");
                         break;
                     default:
@@ -60,3 +71,4 @@ namespace Estructura_Seleccion_Multiple
         }
     }
 }
+
